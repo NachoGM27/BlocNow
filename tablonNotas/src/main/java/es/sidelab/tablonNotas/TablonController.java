f7a65647e71c3c46f14425e026de3d8bd51112aa
JorEquis <jorgecarlin@gmail.com>
2017-02-01 19:14:11 +0100

    TablonController olvidado

--- tablonNotas/src/main/java/es/sidelab/tablonNotas/TablonController.java
+++ tablonNotas/src/main/java/es/sidelab/tablonNotas/TablonController.java
@@ -3,21 +3,24 @@ package es.sidelab.tablonNotas;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class TablonController {
 
+	private List<Nota> notas = new ArrayList<>();
+	
 	public TablonController() {
+		notas.add(new Nota("Prueba 1"));
 	}
 
 	@GetMapping("/")
 	public String tablon(Model model) {
+		
+		model.addAttribute("notas", notas);
+		
 		return "inicio";
 	}
 }
