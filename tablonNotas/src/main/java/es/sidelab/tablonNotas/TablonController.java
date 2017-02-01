327e7be149597d40b39b80eaba43516d1479d591
JorEquis <jorgecarlin@gmail.com>
2017-02-01 19:39:45 +0100

    Implementacion de Form

--- tablonNotas/src/main/java/es/sidelab/tablonNotas/TablonController.java
+++ tablonNotas/src/main/java/es/sidelab/tablonNotas/TablonController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TablonController {
@@ -22,5 +23,20 @@ public class TablonController {
 		model.addAttribute("notas", notas);
 		
 		return "inicio";
+	}
+	
+	@PostMapping("/")
+	public String guardarNota(Model model, Nota nota) {
+
+		notas.add(nota);
+		model.addAttribute("notas", notas);
+
+		return "inicio";
+	}
+	
+	@GetMapping("/CrearNota")
+	public String nuevaNota(Model model){
+		
+		return "CrearNota";
 	}
 }
